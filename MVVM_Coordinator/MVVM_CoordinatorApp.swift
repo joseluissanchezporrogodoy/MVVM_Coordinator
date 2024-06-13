8d4d6085bb718c5f85d3761b82acd9f57d0864e9
José Luis Sánchez-Porro Godoy <jose.sanchezporro@mirada.tv>
2024-06-13 16:19:08 +0200

    Initial Commit

---
+++ MVVM_Coordinator/MVVM_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CoordinatorApp.swift
+//  MVVM_Coordinator
+//
+//  Created by Jose Luis Sánchez-Porro on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
